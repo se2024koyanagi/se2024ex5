bd25a70a3d09db4bf6d226ca414788e9f9e551e8
se2024koyanagi <r202304760bg@jindai.jp>
2025-01-20 01:30:05 +0900

    Add files via upload

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(void) {
+  char str[] = "Hello, World!";
+  int length = strlen(str);
+
+  printf("String: %s\n", str);
+  printf("Length: %d\n", length);
+  return 0;
+}
