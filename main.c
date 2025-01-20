9bd50f84e20afb2da934db8e3d8d4ff4260a541b
se2024koyanagi <r202304760bg@jindai.jp>
2025-01-20 09:15:37 +0900

    aa

--- main.c
+++ main.c
@@ -4,8 +4,10 @@
 int main(void) {
   char str[] = "Hello, World!";
   int length = strlen(str);
+  int i=10;
 
   printf("String: %s\n", str);
   printf("Length: %d\n", length);
+  printf("i=%d",i);
   return 0;
 }
